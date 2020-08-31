9c422bc2d5036c21353c7731f27d2ef08a06d260
Sandor Maraczy <sandoorm@hotmail.com>
2020-08-31 18:23:04 +0200

    first commit

---
+++ Galaxis-WebAPI/Controllers/KukiController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galaxis_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KukiController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "kuki1", "kukiiii2654zgt6ghtghthg5h5ht" };
+        }
+    }
+}
